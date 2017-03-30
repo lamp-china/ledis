6c8caa634c825de8fb825384a9eee3178929ad23
hu jie <hu.jie@vpclub.cn>
2017-03-30 18:58:24 +0800

    0330

---
+++ src/main/java/com/lamp/ledis/configuration/SingleConfiguration.java
@@ -0,0 +1,20 @@
+package com.lamp.ledis.configuration;
+
+import com.lamp.ledis.utils.BasicAttributes;
+
+public class SingleConfiguration {
+	
+	private String host;
+	
+	private int    post = BasicAttributes.DEFAULT_POST;
+	
+	private int    select;
+	
+	private int    connectionMode;
+	
+	private int    netMode;
+	
+	private int    connectionNum;
+	
+	
+}
---
+++ src/main/java/com/lamp/ledis/utils/BasicAttributes.java
@@ -0,0 +1,7 @@
+package com.lamp.ledis.utils;
+
+public class BasicAttributes {
+	public static final int  DEFAULT_POST = 6379;
+	
+	public static final int  DEFAULT_COMMECTION_NUM = Runtime.getRuntime().availableProcessors();
+}
